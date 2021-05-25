b0d5de882ead4b88e0e9f8121f178bf9b90cfc37
RainedX <1355247497@qq.com>
2021-05-25 14:24:25 +0800

    feat:完善install方法

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
-import { render } from 'less';
 import Vue from 'vue';
-import VueRouter from 'vue-router';
+import VueRouter from '../vue-router';
 import Home from '../views/Home.vue';
 
 Vue.use(VueRouter);
--- src/vue-router/install.js
+++ src/vue-router/install.js
@@ -1,1 +1,15 @@
-export default function install(Vue) {}
+export default function install(Vue, options) {
+  // Vue.mixin ===> mergeOptions ===> 生命周期合并为数组
+  Vue.mixin({
+    // 给所有组件的生命周期都增加beforeCreate方法
+    beforeCreate() {
+      // 根实例
+      if (this.$options.router) {
+        this._routerRoot = this; // 将根实例挂载到_routerRoot属性上
+        this._router = this.$options.router; // 将当前router实例挂载在_router上
+      } else {
+        this._routerRoot = this.$parent && this.$parent._routerRoot;
+      }
+    }
+  })
+}
